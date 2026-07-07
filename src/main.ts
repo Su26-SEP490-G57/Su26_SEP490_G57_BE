e7212cedcfe01fbe0c192b687b4f77e8167d1b92
giangdoit123 <giangdoit123@gmail.com>
2026-07-07 17:11:23 +0700

    feat(SEP490-138): nginx workflow [skip ci]

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
